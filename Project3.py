d653ce29f8e78a11cbcef44321d72832916ff454
Andrew <106121781+DeiuCodex@users.noreply.github.com>
2022-05-29 17:45:38 +0300

    YouTube Voice-Searching

--- Project3.py
+++ Project3.py
@@ -25,7 +25,7 @@ def voice(message1):
     en.runAndWait()
 
 
-path = "chromedriver.exe"                                       # Locatia pentru browser
+path = "chromedriver.exe"                                                                             # Locatia pentru browser
 
 voice("Please say a music that you want to search")
 
@@ -38,7 +38,7 @@ def automateYoutube(comanda):
     boo = False
 
     while not boo:
-        speak = sr.Recognizer()                                 # procesul de inregistrare audio
+        speak = sr.Recognizer()                                                                      # procesul de inregistrare audio
 
         try:
             with sr.Microphone() as speaky:
@@ -47,10 +47,10 @@ def automateYoutube(comanda):
                 print("listening...")
 
 
-                searchquery = speak.listen(speaky)              # inregistrarea vocii
+                searchquery = speak.listen(speaky)                                                   # inregistrarea vocii
 
 
-                MyText = speak.recognize_google(searchquery)    # utilizarea Google Speech Recognition pentru prelucrare audio
+                MyText = speak.recognize_google(searchquery)                                         # utilizarea Google Speech Recognition pentru prelucrare audio
                 MyText = MyText.lower()
 
         except sr.RequestError as e:
@@ -73,10 +73,10 @@ def automateYoutube(comanda):
         else:
             boo = True
             print("OK")
-            search_keyword = MyText                                 # stocarea muzicii rostite intr-o variabila
-            search_keyword_clear = search_keyword.replace(' ', '+') # in loc de spatiu dintre cuvinte, se adauga "+" specific sintaxei
+            search_keyword = MyText                                                                 # stocarea muzicii rostite intr-o variabila
+            search_keyword_clear = search_keyword.replace(' ', '+')                                 # in loc de spatiu dintre cuvinte, se adauga "+" specific sintaxei
             html = urllib.request.urlopen(
-                "https://www.youtube.com/results?search_query=" + search_keyword_clear)  # generarea link-ului
+                "https://www.youtube.com/results?search_query=" + search_keyword_clear)             # generarea link-ului
             video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
 
             print("Link-ul generat: https://www.youtube.com/watch?v=" + video_ids[0])
@@ -88,17 +88,17 @@ def automateYoutube(comanda):
             voice("Now I'm ready! Enjoy!")
             url = 'https://www.youtube.com/watch?v=' + video_ids[0]
 
-            driver = webdriver.Chrome(path)                                 # deschiderea browser-ului
-            driver.get(url)                                                 # accesarea link-ului generat
+            driver = webdriver.Chrome(path)                                                         # deschiderea browser-ului
+            driver.get(url)                                                                         # accesarea link-ului generat
 
-            time.sleep(200)                                                 # timp de acces in browser -> 200s
+            time.sleep(200)                                                                         # timp de acces in browser -> 200s
 
 
 
                                                     # @@@ Main Function @@@
 if __name__ == '__main__':
 
-    comenzi = {'search youtube': automateYoutube} #dictionar configurat in prealabil - ( comanda vocala: comanda exec sistem)
+    comenzi = {'search youtube': automateYoutube}                                       #dictionar configurat in prealabil - ( comanda vocala: comanda exec sistem)
     txt = 'search youtube for rammstein' or 'search youtube for lady gaga' or 'search youtube for metallica'
     txt_curat = txt.replace('', '+')
     txt_curat.split('for')
