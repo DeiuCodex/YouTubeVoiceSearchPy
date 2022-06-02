14350890c1281d0696433f1314acb9ad51db6a5d
Andrew <106121781+DeiuCodex@users.noreply.github.com>
2022-06-02 13:13:47 +0300

    Prezentare Proiect
    -

--- Prezentare_Lupsan.py
+++ Prezentare_Lupsan.py
@@ -115,8 +115,28 @@ try:
         txt_curat.split('for')                                                           # se cauta ceea ce s-a rostit dupa " for "
         comanda = ['search youtube', 'rammstein du hast', 'lady gaga poker face',
                    'metallica the unforgiven']                                                  # lista cu melodii
-        executa_comanda = comenzi[comanda[0]]
-        executa_comanda(comanda[1])
+        executa_comanda = comenzi[comanda[0]]                                                   # **
+        executa_comanda(comanda[1])                                                             # ***
 
 except(KeyboardInterrupt, MemoryError, ResourceWarning, ModuleNotFoundError, ConnectionError) as e:
         print("Script-ul a fost inchis de catre utilizator sau din alt motiv.")
+        
+        
+        
+        """
+      
+        * sintaxa YouTube prevede:
+        
+        https://www.youtube.com/results?search_query=rammstein+du+hast (CORECT)
+
+        https://www.youtube.com/results?search_query=rammstein du hast (INCORECT)
+        
+        Folosind fara +, in mod normal in search va interveni doar "rammstein", dar nici in acest caz Python nu reuseste sa interpereteze. 
+        Folosind totusi in browser-ul normal varianta (INCORECT), browser-ul va folosi " %20 " in loc de spatiu si va adauga "+" de la sine, dar aici in cod, e important sa tinem cont de "+" !!!
+        
+        
+        ** se creaza o variabila exec_comanda unde din dictinarul comenzi, folosim primul element "search youtube" (comanda[0] )
+        
+        *** se executa elementul 1 din lista comanda, anume rammstein du hast. tinand cont si de influenta dictionarului predefinit. Unde se executa comanda vocala (de catre utilizator) si comanda executata de sistem (automateYoutube)
+        
+        """
